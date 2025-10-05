09e0da444d048415ed51c7467521a50b08fdfd57
Sebastián G <seba.gallardog@duocuc.cl>
2025-10-04 23:15:32 -0300

    Team collection

---
+++ src/collections/Team.ts
@@ -0,0 +1,45 @@
+import type { CollectionConfig } from 'payload'
+
+export const Team: CollectionConfig = {
+  slug: 'team',
+  admin: {
+    group: {
+      en: 'Website Content',
+      es: 'Contenido del Sitio Web'
+    }
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      label: {
+        en: `Member name`,
+        es: 'Nombre del profesional'
+      },
+      required: true
+    },
+    {
+      name: 'role',
+      type: 'text',
+      label: {
+        en: `Member role`,
+        es: 'Rol del profesional'
+      }
+    },
+    {
+      name: 'picture',
+      type: 'upload',
+      relationTo: 'media',
+    }
+  ],
+  labels: {
+    singular: {
+      en: 'team member',
+      es: 'profesional'
+    },
+    plural: {
+      en: 'Team',
+      es: 'Profesionales'
+    }
+  }
+}
