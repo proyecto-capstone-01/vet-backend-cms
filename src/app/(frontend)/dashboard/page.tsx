4758744af9f321f7a222fe71dbd4ffdea05c5712
Sebastián G <seba.gallardog@duocuc.cl>
2025-10-04 23:21:36 -0300

    Dashboard entry point

---
+++ src/app/(frontend)/dashboard/page.tsx
@@ -0,0 +1,25 @@
+import { isDashboardUser } from '@/access/isDashboardUser'
+import { getPayload } from 'payload'
+import configPromise from '@payload-config'
+import { headers as getHeaders } from 'next/headers.js'
+import config from '@/payload.config'
+
+export default async function Home(){
+  const headers = await getHeaders()
+  const payloadConfig = await config
+  const payload = await getPayload({ config: payloadConfig })
+  const { user } = await payload.auth({ headers })
+
+  //console.log(user)
+
+  return (
+    <div>
+      <div>
+        Dashboard homepage
+      </div>
+      {user &&
+        <span>Hello {user?.name}</span>
+      }
+    </div>
+  )
+}
