dc7abedf85dee8f63aaaeddc1afa69a37cdbdef4
Sebastián G <seba.gallardog@duocuc.cl>
2025-10-04 23:20:32 -0300

    Payload config

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,1 +1,5 @@
-export const importMap = {}
+import { default as default_4cf97da56fb20df582c9b1a4377ed4f5 } from '@/components/beforeDashboard'
+
+export const importMap = {
+  "@/components/beforeDashboard#default": default_4cf97da56fb20df582c9b1a4377ed4f5
+}
