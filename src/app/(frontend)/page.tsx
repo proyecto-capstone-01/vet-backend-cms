bd68158556b63539400840a5654eec13905fcff5
Sebastián G <seba.gallardog@duocuc.cl>
2025-10-16 03:21:38 -0300

    clean unused code

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,10 +1,8 @@
 import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
 import { getPayload } from 'payload'
 import React from 'react'
 
 import config from '@payload-config'
-import './styles.css'
 import { redirect } from 'next/navigation'
 
 export default async function HomePage() {
@@ -33,30 +31,6 @@ export default async function HomePage() {
   })
 
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a className="admin" href={payloadConfig.routes.admin} rel="noopener noreferrer">
-            Go to CMS panel
-          </a>
-          <a className="docs" href="/dashboard">
-            Go to Dashboard
-          </a>
-        </div>
-      </div>
-      <div className="footer">
-      </div>
-    </div>
+    <div className="home"></div>
   )
 }
