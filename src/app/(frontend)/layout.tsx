ed3b6b5e63cec09c96b4214466558442195d457b
Sebastián G <seba.gallardog@duocuc.cl>
2025-10-16 03:22:55 -0300

    Tailwind and next-team implementation

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -5,6 +5,7 @@ import { headers as getHeaders } from 'next/headers'
 import config from '@payload-config'
 import { getPayload } from 'payload'
 import { redirect } from 'next/navigation'
+import { ThemeProvider } from "@/components/ThemeProvider"
 
 export const metadata: Metadata = {
   description: 'app',
@@ -18,15 +19,22 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   const payload = await getPayload({ config: payloadConfig })
   const { user } = await payload.auth({ headers })
 
+  console.log('main layout ran')
+
   if (!user) return redirect('/admin/login?redirect=%2F')
 
-
-
   return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
-    </html>
+      <html lang="en">
+        <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        </body>
+      </html>
   )
 }
