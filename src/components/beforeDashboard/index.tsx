53704e7818273b72e009dd9c6e3943dd0df473b6
Sebastián G <seba.gallardog@duocuc.cl>
2025-10-18 16:23:40 -0300

    feat: Link to dashboard from cms

--- src/components/beforeDashboard/index.tsx
+++ src/components/beforeDashboard/index.tsx
@@ -2,64 +2,43 @@ import { Banner } from '@payloadcms/ui/elements/Banner'
 import React from 'react'
 import { getTranslation, I18n } from '@payloadcms/translations'
 import './index.scss'
+import { IconExternalLink, IconHammer } from '@tabler/icons-react'
+import { Button } from '@payloadcms/ui'
+import Link from 'next/link'
 
 const baseClass = 'before-dashboard'
 
-export default async function BeforeDashboard({ i18n } : { i18n: Pick<I18n<any, any>, "fallbackLanguage" | "language" | "t">}) {
-  const tl = getTranslation(i18n.t('custom:dashboardGreeting'), i18n)
+export default async function BeforeDashboard({
+  i18n,
+}: {
+  i18n: Pick<I18n<any, any>, 'fallbackLanguage' | 'language' | 't'>
+}) {
+  const dg = getTranslation(i18n.t('custom:dashboardGreeting'), i18n)
+  const cmsm = getTranslation(i18n.t('custom:cmsMessage'), i18n)
+  const gtd = getTranslation(i18n.t('custom:goToDashboard'), i18n)
+  const rbw = getTranslation(i18n.t('custom:rebuildWebsite'), i18n)
 
   return (
     <div className={baseClass}>
       <Banner className={`${baseClass}__banner`} type="success">
-        <h4>{tl}</h4>
+        <h4>{dg}</h4>
       </Banner>
-      Here&apos;s what to do next:
-      <ul className={`${baseClass}__instructions`}>
-        <li>
-          {' with a few pages, posts, and projects to jump-start your new site, then '}
-          <a href="/" target="_blank">
-            visit your website
-          </a>
-          {' to see the results.'}
-        </li>
-        <li>
-          If you created this repo using Payload Cloud, head over to GitHub and clone it to your
-          local machine. It will be under the <i>GitHub Scope</i> that you selected when creating
-          this project.
-        </li>
-        <li>
-          {'Modify your '}
-          <a
-            href="https://payloadcms.com/docs/configuration/collections"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            collections
-          </a>
-          {' and add more '}
-          <a
-            href="https://payloadcms.com/docs/fields/overview"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            fields
-          </a>
-          {' as needed. If you are new to Payload, we also recommend you check out the '}
-          <a
-            href="https://payloadcms.com/docs/getting-started/what-is-payload"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Getting Started
-          </a>
-          {' docs.'}
-        </li>
-        <li>
-          Commit and push your changes to the repository to trigger a redeployment of your project.
-        </li>
-      </ul>
+      <p>{cmsm}</p>
 
+      <div className={`${baseClass}__actions`}>
+        <Link href="/dashboard" prefetch='auto'>
+          <Button>
+            {gtd}
+            <IconExternalLink size={16} style={{ marginLeft: '8px' }} />
+          </Button>
+        </Link>
+
+        <Button >
+          {rbw}
+          <IconHammer size={16} style={{ marginLeft: '8px' }} />
+        </Button>
+
+      </div>
     </div>
   )
 }
-
--- src/custom-translations.ts
+++ src/custom-translations.ts
@@ -8,6 +8,9 @@ export const customTranslations = {
       dashboardGreeting: `Welcome to the Content Management System Dashboard.`,
       unauthorizedTitle: `You don't have access to this content.`,
       unauthorizedSubtitle: 'Ask an administrator for permissions',
+      cmsMessage: 'You are in your website content manager.',
+      goToDashboard: 'Go to dashboard',
+      rebuildWebsite: 'Rebuild website',
     }
   },
   es: {
@@ -15,6 +18,9 @@ export const customTranslations = {
       dashboardGreeting: 'Bienvenido al panel de Gestión de Contenido.',
       unauthorizedTitle: 'No tienes acceso a este contenido.',
       unauthorizedSubtitle: 'Consulta con el administrador.',
+      cmsMessage: 'Para sincronizar los cambios con tu sitio web, haz clic en el botón «Reconstruir sitio web».',
+      goToDashboard: 'Ir al dashboard',
+      rebuildWebsite: 'Reconstruir sitio web',
     }
   }
 }
