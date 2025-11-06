03739372ec3fb623acdb5f2b9ab469c51076dd95
Sebastián G <seba.gallardog@duocuc.cl>
2025-11-06 01:47:14 -0300

    feat: Allow higher role access

--- src/access/isDashboardUser.ts
+++ src/access/isDashboardUser.ts
@@ -1,11 +1,11 @@
 import { Access, FieldAccess } from 'payload';
 
-// Return true or false based on if the user has the dashboard role
+// Return true or false based on if the user has the dashboard role or higher (admin)
 export const isDashboardUser: Access = ({ req: { user } }) => {
-  return Boolean(user?.roles.includes('dashboard'));
+  return Boolean(user?.roles.includes('dashboard') || user?.roles.includes('admin'));
 }
 
-// Return true or false based on if the user has an admin role
-// export const isAdminFieldLevel: FieldAccess = ({ req: { user } }) => {
-//   return Boolean(user?.roles?.includes('admin'));
-// }
+// Return true or false based on if the user has the dashboard role or higher (admin) for field-level access
+export const isDashboardUserFieldLevel: FieldAccess = ({ req: { user } }) => {
+  return Boolean(user?.roles?.includes('dashboard') || user?.roles?.includes('admin'));
+}
